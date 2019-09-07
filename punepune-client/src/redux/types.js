956abff48e353ab7380bfb26513a908047f3d568
gentixharavina <gentixharavina@gmail.com>
2019-09-08 00:54:29 +0200

    types

--- punepune-client/src/redux/types.js
+++ punepune-client/src/redux/types.js
@@ -11,7 +11,7 @@ export const LOADING_DATA = 'LOADING_DATA'
 export const STOP_LOADING_UI = 'STOP_LOADING_UI'
 //data reducer types
 export const SET_POSTS = 'SET_POSTS'
-export const SET_POST = 'SET_POT'
+export const SET_POST = 'SET_POST'
 export const LIKE_POST = 'LIKE_POST'
 export const UNLIKE_POST = 'UNLIKE_POST'
 export const DELETE_POST = 'DELETE_POST'
